82cabfcdf3df50e6012e0d9185af9d29cf9d9663
Orwellian <orwellian225@gmail.com>
2023-11-28 18:05:24 +0200

    add: Primitive 3D scene and controls

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,50 @@
-
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy_egui::EguiPlugin;
+use bevy_flycam::{PlayerPlugin, KeyBindings};
+
+mod maze;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
-        .add_systems(Update, ui_example)
+        .add_plugins(PlayerPlugin)
+        .insert_resource(KeyBindings {
+            toggle_grab_cursor: KeyCode::C,
+            ..default()
+        })
+        .add_systems(Startup, setup)
         .run();
 }
 
-fn ui_example(mut ctx: EguiContexts) {
-    egui::Window::new("Wits MicroMouse Simulator").show(ctx.ctx_mut(), |ui| {
-        ui.label("Hello, World!")
-    }
+fn setup(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut commands: Commands
+) {
+    // Light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4., 8., 4.),
+        ..default()
+    });
 
-    );
+    // Floor
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Box::new(5.0, 0.4, 5.).into()),
+        material: materials.add(Color::DARK_GREEN.into()),
+        ..default()
+    });
+
+    // Origin Marker
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube{ size: 1.0 })),
+        material: materials.add(Color::WHITE.into()),
+        transform: Transform::from_xyz(0., 0.5, 0.),
+        ..default()
+    });
 }
